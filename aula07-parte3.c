f9f527537dd8fd1c14235201418683c374af9e9e
glaucotodesco <glauco.todesco@gmail.com>
2020-05-26 08:49:09 -0300

    Update aula07-parte3.c
    Fix if/else - aula07

--- aula07-parte3.c
+++ aula07-parte3.c
@@ -1,14 +1,14 @@
 /*
-   Calcular o IMC (Õndice de Massa Corporal) de uma pessoa
-   A fÛrmula do IMC È: Peso (kg) / (Altura * Altura) (m).
-   Informe a classificaÁ„o do IMC na tela de acordo com as regras abaixo:
+   Calcular o IMC (√çndice de Massa Corporal) de uma pessoa
+   A f√≥rmula do IMC √©: Peso (kg) / (Altura * Altura) (m).
+   Informe a classifica√ß√£o do IMC na tela de acordo com as regras abaixo:
    
    IMC < 18,5 Abaixo do peso 
-   De 18,5 a menor que 25,0 Saud·vel 
+   De 18,5 a menor que 25,0 Saud√°vel 
    De 25,0 a menor que 30,0 Sobrepeso 
    De 30,0 a menor que 35,0 Obesidade Grau I 
    De 35,0 a menor que 40,0 Obesidade Grau II (severa) 
-   De 40,0 em diante Obesidade Grau III (mÛrbida) 
+   De 40,0 em diante Obesidade Grau III (m√≥rbida) 
    
 */
 
@@ -26,25 +26,25 @@ int main()
 	
 	imc = peso / (altura * altura);
 	
-	if(peso > 0)
+	if(imc > 0)
 	{
-		if(peso < 18.5){
+		if(imc < 18.5){
 			printf("Abaixo do peso");
 		}
 		else{
-			if(peso < 25){
+			if(imc < 25){
 				printf("Saudavel");
 			}
 			else{
-				if(peso < 30){
+				if(imc < 30){
 					printf("Sobrepeso");
 				}
 				else{
-					if(peso < 35){
+					if(imc < 35){
 						printf("Obsidade Grau I");
 					}
 					else{
-						if(peso < 40){
+						if(imc < 40){
 							printf("Obsidade Grau II");
 						}
 						else
@@ -57,7 +57,7 @@ int main()
 		}
 	}
 	else{
-		printf("Peso deve ser positivo!");
+		printf("Peso e altura devem ser positivos!");
 	}
 	
 	
